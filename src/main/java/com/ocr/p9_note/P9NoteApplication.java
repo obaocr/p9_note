8c51fdd57644ed8ea78d612a31b61f7899067497
BARBERIS Olivier <olivier.barberis@axa.fr>
2021-01-04 19:48:08 +0100

    init

---
+++ src/main/java/com/ocr/p9_note/P9NoteApplication.java
@@ -0,0 +1,13 @@
+package com.ocr.p9_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P9NoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P9NoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocr/p9_note/P9NoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocr.p9_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P9NoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
